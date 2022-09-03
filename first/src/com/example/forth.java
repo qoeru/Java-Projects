4850d2dda6fac54fa99beb7897d47f9337723449
Qoeru <66938523+qoeru@users.noreply.github.com>
2022-09-03 18:19:39 +0300

    Правка к первой практике

--- first/src/com/example/forth.java
+++ first/src/com/example/forth.java
@@ -1,21 +1,30 @@
 package com.example;
 import java.util.Random;
+import java.util.Arrays;
+
 //случайные числа ([0, 100]) двумя методами
 public class forth {
+    public static void printArray(int[] a, int len){
+        for(int i = 0; i < len; i++){
+            System.out.println(a[i]);
+        }
+    }
     public static void main(String[] args) {
         int[] a = new int[5];
         System.out.println("Первый способ");
         for(int i = 0; i < 5; i++) {
             a[i] = (int) (Math.random() * 101);
-            System.out.println(a[i]);
         }
+        Arrays.sort(a);
+        printArray(a, 5);
 
         System.out.println("Второй способ");
         Random rnd = new Random();
 
         for(int i = 0; i < 5; i++) {
             a[i] = rnd.nextInt(100) + 1;
-            System.out.println(a[i]);
         }
+        Arrays.sort(a);
+        printArray(a, 5);
     }
 }
